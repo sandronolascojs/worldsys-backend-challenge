445f069b4f14c21ab2c63a62f34bf632473eb16e
Sandro Nolasco <sandronolasco1@gmail.com>
2025-07-09 05:07:08 -0300

    fix: add port to db connection

--- src/lib/db.lib.ts
+++ src/lib/db.lib.ts
@@ -8,12 +8,15 @@ export async function getDbPool(): Promise<mssql.ConnectionPool> {
   if (_pool && _pool.connected) {
     return _pool;
   }
+
   _pool = await mssql.connect({
     user: env.DB_USER,
     password: env.DB_PASSWORD,
     server: env.DB_HOST,
     database: env.DB_NAME,
+    port: env.DB_PORT,
     options: { encrypt: isProduction, trustServerCertificate: !isProduction },
   });
+
   return _pool;
 }
