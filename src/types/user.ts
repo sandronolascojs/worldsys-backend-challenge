a7eb5ed4939db66bc7c41cd978d5eab5f5d2528e
Sandro Nolasco <sandronolasco1@gmail.com>
2025-07-09 05:06:12 -0300

    feat: add date validation to avoid break db insert

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,10 +1,15 @@
 import z from 'zod';
 
+const minSqlDate = new Date('0001-01-01');
+const maxSqlDate = new Date('9999-12-31');
+
 export const userToInsertSchema = z.object({
   NombreCompleto: z.string().min(1).max(100),
-  DNI: z.number().int().positive(),
+  DNI: z.coerce.number().int().positive(),
   Estado: z.string().min(1).max(10),
-  FechaIngreso: z.date(),
+  FechaIngreso: z.date().refine((date) => date >= minSqlDate && date <= maxSqlDate, {
+    message: 'FechaIngreso out of SQL Server date range',
+  }),
   EsPEP: z.boolean(),
   EsSujetoObligado: z.boolean().nullable(),
 });
