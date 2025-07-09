5f5292688ca9ce063b8a0ea7535595e5cc3fe84c
Sandro Nolasco <sandronolasco1@gmail.com>
2025-07-09 01:54:59 -0300

    feat: improve pool connection quality

--- src/lib/db.lib.ts
+++ src/lib/db.lib.ts
@@ -1,17 +1,19 @@
-import { env } from 'config/env.config';
 import mssql from 'mssql';
+import { env } from '../config/env.config';
 
+let _pool: mssql.ConnectionPool | null = null;
 const isProduction = env.APP_ENV === 'production';
 
-const pool = new mssql.ConnectionPool({
-  user: env.DB_USER,
-  password: env.DB_PASSWORD,
-  server: env.DB_HOST,
-  database: env.DB_NAME,
-  options: {
-    encrypt: isProduction,
-    trustServerCertificate: !isProduction,
-  },
-});
-
-export default pool;
+export async function getDbPool(): Promise<mssql.ConnectionPool> {
+  if (_pool && _pool.connected) {
+    return _pool;
+  }
+  _pool = await mssql.connect({
+    user: env.DB_USER,
+    password: env.DB_PASSWORD,
+    server: env.DB_HOST,
+    database: env.DB_NAME,
+    options: { encrypt: isProduction, trustServerCertificate: !isProduction },
+  });
+  return _pool;
+}
